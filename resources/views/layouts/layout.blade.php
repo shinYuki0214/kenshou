6ee55a47d250dcf2472e9f1d00b07d23ee149a5c
岩田真次郎 <iwata@gurukun-web.com>
2023-02-01 07:48:35 +0900

    after build

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
-
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
     </style>
 </head>
@@ -21,22 +21,24 @@
     </main>
 
     @include('parts.footer')
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
-    <script>
-        $(function() {
-            if (window.matchMedia("(max-width: 1023px)").matches) {
+    <script type="module">
 
-                $('.js__nav-btn').click(function() {
-                    $(this).toggleClass('js__nav-active');
-                    if ($('.js__nav-area').hasClass('js__nav-active')) {
-                        $('.js__nav-area').removeClass('js__nav-active');
-                    } else {
-                        $('.js__nav-area').addClass('js__nav-active');
-                    }
-                });
-            } else {};
-        });
-    </script>
+            $(function() {
+                if (window.matchMedia("(max-width: 1023px)").matches) {
+                
+            $('.js__nav-btn').click(function(){
+                $(this).toggleClass('js__nav-active');
+                if($('.js__nav-area').hasClass('js__nav-active')){
+                    $('.js__nav-area').removeClass('js__nav-active');
+                }else{
+                    $('.js__nav-area').addClass('js__nav-active');
+                }
+            });
+                } else {
+                };
+            });
+
+        </script>
 </body>
 
 </html>
