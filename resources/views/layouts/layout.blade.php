0b072d89444771d46e930be64e50bcbdb5dd4261
shinYuki0214 <sin2022web@gmail.com>
2023-02-04 18:15:23 +0900

    font add

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,10 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&family=Noto+Serif+JP:wght@200;300;400;500;600;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <style>
-    </style>
 </head>
 
 <body>
